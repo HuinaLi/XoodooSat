ef1f865ef0f3bd0f0a244b85f2ca38dd7d85fe28
lhn <lihuina116@gmail.com>
2022-01-09 21:27:25 +0800

    Update xoodooRound.cpp

--- xoodooRound.cpp
+++ xoodooRound.cpp
@@ -11,7 +11,7 @@ using namespace std::chrono;
 using namespace XOODOOSAT;
 
 XoodooRound::XoodooRound(int analy_mode, int rounds, int weight, int thread, int mode) :solver() {
-    analysis_mode = analysis_mode;
+    analysis_mode = analy_mode;
     AS_weight_num = weight;
     AS_state_num = rounds;
     AS_mode = mode;
